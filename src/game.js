78376ac0db2292f9fdd01eea10d4488f9ea03887
William Pheloung <wpheloung@gmail.com>
2014-10-01 17:22:59 -0400

    Basic commit of game, click to move functionality prototyped

--- src/game.js
+++ src/game.js
@@ -3,3 +3,174 @@
  *
  *
  */
+ var game = cc.Scene.extend({ //setting up the scene object
+	ctor:function(difficulty, characters) { //the scene constructor
+		this._super();
+	},
+	
+	onEnter:function(){ //this is called right after ctor, generate layers here	
+		this._super();		//var fanatsyBackground = new FantasyBackgroundLayer();
+		var demoLayer = new ActionsDemoLayer();
+		this.addChild(demoLayer); 
+		//var fantasyInteractiveLayer = new FantasyInteractiveLayer();
+		//this.addChild(fantasyInteractiveLayer);
+	}
+ });
+ 
+ var ActionsDemoLayer = cc.Layer.extend ({
+	player:null,
+	ctor:function(){
+		this._super();
+		this.player = new cc.Sprite("src/grossini.png");
+		this.addChild(this.player);
+		this.player.setPosition(0,0);
+		var action = cc.moveTo(1,cc.p(100,200));
+		this.player.runAction(action);
+		
+		cc.eventManager.addListener ({
+			event: cc.EventListener.MOUSE,
+			onMouseDown: function(event) {
+				event.getCurrentTarget().moveIt(event.getLocation());
+			}
+		},this);
+	},
+	moveIt:function(p) {
+		var action = cc.jumpTo (4,p,100,4);
+		this.player.runAction(action);
+	}
+ });
+ 
+var FantasyBackgroundLayer = cc.Layer.extend({
+	ctor:function() {
+		this._super();
+		var helloworld = cc.Sprite.create("../Assets/art/real/sprites/dialogue_box.png");
+		helloworld.x = cc.director.getWinSize().width/2;
+		helloworld.y = cc.director.getWinSize().height/2;
+		this.addChild(helloworld);
+	}
+ });
+ 
+ var FantasyInteractiveLayer = cc.Layer.extend({
+	player:null,
+	ctor:function() {
+		this._super();
+		// touch bubble
+		/*
+		var parent = this;
+		var touchBubble = cc.Sprite.create("../Assets/art/real/sprites/click_0.png" );
+		
+		var touchBubbleAnim = cc.Animation.create();
+		for ( var i = 0 ; i < 9 ; i ++ ) {
+			var frameName = "../Assets/art/real/sprites/click_" + i + ".png" ;
+			touchBubbleAnim. addSpriteFrameWithFile ( frameName ) ;
+		}
+		touchBubbleAnim.setDelayPerUnit ( 1 / 60 ) ;
+		touchBubbleAnim.setRestoreOriginalFrame ( false ) ;
+		*/
+		
+        this.player = new cc.Sprite("src/grossini.png");
+		
+        this.addChild(this.player/*, 0, TAG_SPRITE*/);
+		
+        this.player.x = 200;
+	    this.player.y = 150;
+
+		var action = cc.moveTo(1,cc.p(100,200));
+		this.player.runAction(action);
+
+		
+		// touch listener
+		
+		var touchListener = cc.EventListener.create({
+			event: cc.EventListener.MOUSE,
+			swallowTouches: true,
+			onMouseDown: function (event) {
+				var action = cc.Animate.create(touchBubbleAnim)
+				touchBubble.runAction( action );
+				return false;
+			}, 
+			onMouseMove: function(event) {
+				touchBubble.x = event._x;
+				touchBubble.y = event._y;
+				return false;
+			}, 
+			onMouseUp: function(event) {
+				//var action = cc.Animate.create(touchBubbleAnim);
+				//touchBubble.runAction( action.reverse() );
+				event.getCurrentTarget().parent.click(event.getLocation());
+				return false;
+			}
+		});
+
+		// attach listener to touch bubble
+		
+		//cc.eventManager.addListener(touchListener, touchBubble);
+		//this.addChild(touchBubble);
+	
+	},
+	onEnter:function() {
+			var foo = cc.moveTo(1,cc.p(300,300));
+		this.player.runAction(foo);
+		cc.log("onEnter");
+        //this.player.runAction(cc.jumpTo(4, cc.p(300, 48), 100, 4));
+	},
+	click:function(p){
+		console.log("click x:" + p.x + " y:" + p.y);
+		//this.player.setPosition(p);
+		this.player.runAction(cc.moveTo(1, p));
+
+	}
+	
+});
+
+//copied directly from demo code
+
+var TestScene = cc.Scene.extend({
+    ctor:function (bPortrait) {
+        this._super();
+        this.init();
+
+        var label = cc.LabelTTF.create("Main Menu", "Arial", 20);
+        var menuItem = cc.MenuItemLabel.create(label, this.onMainMenuCallback, this);
+
+        var menu = cc.Menu.create(menuItem);
+        menu.x = 0;
+        menu.y = 0;
+        menuItem.x = winSize.width - 50;
+        menuItem.y = 25;
+
+        if(!window.sideIndexBar){
+            this.addChild(menu, 1);
+        }
+    },
+    onMainMenuCallback:function () {
+        var scene = cc.Scene.create();
+        var layer = new TestController();
+        scene.addChild(layer);
+        var transition = cc.TransitionProgressRadialCCW.create(0.5,scene);
+		
+		var ts = new TestScene();
+		
+        director.runScene(ts);
+    },
+
+    runThisTest:function () {
+        // override me
+    }
+
+});
+
+/*var ClickAndMoveTestScene = TestScene.extend({
+    runThisTest:function () {
+        var layer = new MainLayer();
+
+        this.addChild(layer);
+        director.runScene(this);
+    }
+});	*/
+
+
+//end direct copy from click demo
+ 
+ var FantasyParallaxLayer;
+ var FantasyInteractiveLayer; 
