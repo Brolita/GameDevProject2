32cc467c47505067373206ed3ba8090db5bc5c4d
William Pheloung <wpheloung@gmail.com>
2014-10-07 19:15:14 -0400

    Enemy Spawning (minus the final boss) completed

--- src/game.js
+++ src/game.js
@@ -508,6 +508,9 @@ var myTestScene = cc.Scene.extend({
 	
 	addEnemies:function(){
 		//remove all the enemies
+		for(i = 0; i < this.collisionMaster.enemies.length; i++){
+			this.collisionMaster.enemies[i].x = -50;
+		}
 		
 		
 		cc.log("511: adding enemies");
@@ -516,70 +519,70 @@ var myTestScene = cc.Scene.extend({
 				break;
 			case 1: //A
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[0].x = 800;
-				this.collisionMaster.enemies[0].y = 300;
-				this.addChild(this.collisionMaster.enemies[0]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 800;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				break;
 			case 2: //AA
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[0].x = 800;
-				this.collisionMaster.enemies[0].y = 300;
-				this.addChild(this.collisionMaster.enemies[0]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 800;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[1].x = 900;
-				this.collisionMaster.enemies[1].y = 300;
-				this.addChild(this.collisionMaster.enemies[1]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 900;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				break;
 			case 3: //B
 				this.collisionMaster.enemies.push(createEnemyB(this));
-				this.collisionMaster.enemies[0].x = 800;
-				this.collisionMaster.enemies[0].y = 300;
-				this.addChild(this.collisionMaster.enemies[0]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 800;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				break;
 			case 4: //AB
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[0].x = 800;
-				this.collisionMaster.enemies[0].y = 300;
-				this.addChild(this.collisionMaster.enemies[0]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 800;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				this.collisionMaster.enemies.push(createEnemyB(this));
-				this.collisionMaster.enemies[1].x = 900;
-				this.collisionMaster.enemies[1].y = 300;
-				this.addChild(this.collisionMaster.enemies[1]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 900;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				break;
 			case 5:  //AAB
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[0].x = 800;
-				this.collisionMaster.enemies[0].y = 300;
-				this.addChild(this.collisionMaster.enemies[0]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 800;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[1].x = 900;
-				this.collisionMaster.enemies[1].y = 300;
-				this.addChild(this.collisionMaster.enemies[1]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 900;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				this.collisionMaster.enemies.push(createEnemyB(this));
-				this.collisionMaster.enemies[2].x = 1000;
-				this.collisionMaster.enemies[2].y = 300;
-				this.addChild(this.collisionMaster.enemies[2]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 1000;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				break;
 			case 6: // ABB
 				this.collisionMaster.enemies.push(createEnemyA(this));
-				this.collisionMaster.enemies[0].x = 800;
-				this.collisionMaster.enemies[0].y = 300;
-				this.addChild(this.collisionMaster.enemies[0]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 800;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				this.collisionMaster.enemies.push(createEnemyB(this));
-				this.collisionMaster.enemies[1].x = 900;
-				this.collisionMaster.enemies[1].y = 300;
-				this.addChild(this.collisionMaster.enemies[1]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 900;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				
 				this.collisionMaster.enemies.push(createEnemyB(this));
-				this.collisionMaster.enemies[2].x = 1000;
-				this.collisionMaster.enemies[2].y = 300;
-				this.addChild(this.collisionMaster.enemies[2]);
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].x = 1000;
+				this.collisionMaster.enemies[this.collisionMaster.enemies.length-1].y = 300;
+				this.addChild(this.collisionMaster.enemies[this.collisionMaster.enemies.length-1]);
 				break;
 			case 7: //empty
 				break;
